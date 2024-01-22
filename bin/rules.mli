9ece9c1e48f3d204e997863a98d9c7d5322ed677
runehash <deangogolan@gmail.com>
2024-01-21 20:28:59 -0800

    reformat code

--- bin/rules.mli
+++ bin/rules.mli
@@ -1,7 +1,5 @@
 open Line
 
-
 val is_valid : (int -> line) -> int -> bool
-
 
 val all_valid : (int, line) Hashtbl.t -> bool
--- src/main.ml
+++ src/main.ml
@@ -1,35 +1,43 @@
 open Ast
 
-
 let parse s =
   let lexbuf = Lexing.from_string s in
   match Parser.prog Lexer.read lexbuf with
-    | None -> failwith "Empty input"
-    | Some e -> e
-
+  | None ->
+      failwith "Empty input"
+  | Some e ->
+      e
 
 let rec eval = function
-    | `Conn (binop, exp1, exp2) -> collapse binop (eval exp1) (eval exp2)
-    | `Not (`Bool a) -> `Bool (not a)
-    | `Not exp -> negate exp
-    | `Atom _ as atom -> atom 
-    | `Bool _ as bool -> bool
+  | `Conn (binop, exp1, exp2) ->
+      collapse binop (eval exp1) (eval exp2)
+  | `Not (`Bool a) ->
+      `Bool (not a)
+  | `Not exp ->
+      negate exp
+  | `Atom _ as atom ->
+      atom
+  | `Bool _ as bool ->
+      bool
 
-and collapse binop exp1 exp2 = match binop, exp1, exp2 with
-    | binop, `Bool a, `Bool b -> begin 
-        match binop with
-        | `And -> `Bool (a && b)
-        | `Or -> `Bool (a || b)
-        | `Impl -> `Bool ((not a) || b)
-        | `Iff -> `Bool (a = b)
-    end
-    | _ -> `Conn (binop, exp1, exp2) 
+and collapse binop exp1 exp2 =
+  match (binop, exp1, exp2) with
+  | binop, `Bool a, `Bool b -> (
+    match binop with
+    | `And ->
+        `Bool (a && b)
+    | `Or ->
+        `Bool (a || b)
+    | `Impl ->
+        `Bool ((not a) || b)
+    | `Iff ->
+        `Bool (a = b) )
+  | _ ->
+      `Conn (binop, exp1, exp2)
 
-and negate exp = match eval exp with
-    | `Bool a -> `Bool (not a)
-    | exp -> `Not exp
-
+and negate exp =
+  match eval exp with `Bool a -> `Bool (not a) | exp -> `Not exp
 
 let interp s =
-    let line = parse s in
-    {prop = eval line.prop; derivedby = line.derivedby}
+  let line = parse s in
+  {prop= eval line.prop; derivedby= line.derivedby}
